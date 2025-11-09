a446b9de1892abf7a81f518b7fe7c7215c9d7532
Kade Whitman <kwhitman1@muskingum.edu>
2025-11-09 13:46:17 -0500

    add presets to babel.config.js just to get the app styling working again.

--- ScavengerHunt/babel.config.js
+++ ScavengerHunt/babel.config.js
@@ -1,7 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    // nativewind exposes a babel config (with nested plugins), so add it
+    // as a preset rather than a plugin to avoid Babel validation errors.
+    presets: ['babel-preset-expo', 'nativewind/babel'],
+    plugins: [],
   };
 };
      
